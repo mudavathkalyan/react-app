c5ea9892adc6a637d31e49ba6085b1b33616b247
kalyan <mudavathkalyan26@gmail.com>
2026-05-08 10:22:08 +0530

    About section added..

--- src/About.jsx
+++ src/About.jsx
@@ -7,7 +7,11 @@ function About() {
 
             {/* go to home page */}
 
-            <a href="/">Home</a>
+           <p> Go to Home page by </p> <a href="/">click</a>
+<br />
+<br />
+
+           <p>All about as is here</p>
         </div>
     )
 }
