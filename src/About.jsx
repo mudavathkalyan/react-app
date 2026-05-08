60088cc509ab3c5ebb94ac9e3396fb0eac7beeec
kalyan <mudavathkalyan26@gmail.com>
2026-05-08 11:59:33 +0530

    new commit at about

--- src/About.jsx
+++ src/About.jsx
@@ -7,11 +7,11 @@ function About() {
 
             {/* go to home page */}
 
-           <p> Go to Home page by </p> <a href="/">click</a>
-<br />
-<br />
+            <p> Go to Home page by </p> <a href="/">click</a>
+            <br />
+            <br />
 
-           <p>All about as is here</p>
+            <p>All about as is here, okay..</p>
         </div>
     )
 }
